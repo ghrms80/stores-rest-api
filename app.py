527e12b17903970c97be961f284339c5e9d2696b
jiho <ghrms80@gmail.com>
2020-07-27 16:55:42 +0900

    Added PostgreSQL environment variable and psycopg2.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT, jwt_required
@@ -6,20 +8,20 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
-from db import db
+# from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # 다양한 sql db에서 동작
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # 다양한 sql db에서 동작
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False        # mysql, sqlite, postgresql...
 app.secret_key = 'jiho'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
